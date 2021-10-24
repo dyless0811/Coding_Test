527ec0a78a437877077fc7a2641d6b5500c9c1d1
dyless0811 <dyless0811@gmail.com>
2021-10-24 14:35:08 +0900

    프로그래머스 - 로또의 최고 순위와 최저 순위

--- Algorithm_Practice/src/programmers/lv1/N77484.java
+++ Algorithm_Practice/src/programmers/lv1/N77484.java
@@ -1,7 +1,12 @@
 package programmers.lv1;
 
 public class N77484 {
-	public int[] solution(int[] lottos, int[] win_nums) {
+	public static void main(String[] args) {
+		int[] lottos = {44, 1, 0, 0, 31, 25};
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		solution(lottos, win_nums);
+	}
+	public static int[] solution(int[] lottos, int[] win_nums) {
 		int hRank = 6, lRank = 6, hCount = 0, lCount = 0, zero = 0;
         
 		for(int l : lottos) {
