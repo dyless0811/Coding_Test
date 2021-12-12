52e19629fc1d93faafa1d6f0e58b4ad7ba9a5220
dyless0811 <dyless0811@gmail.com>
2021-12-12 16:42:27 +0900

    프로그래머스 - 124 나라의 숫자

--- Algorithm_Practice/src/programmers/lv2/N12899.java
+++ Algorithm_Practice/src/programmers/lv2/N12899.java
@@ -1,8 +1,10 @@
 package programmers.lv2;
 
+import java.util.ArrayList;
+
 public class N12899 {
     public static void main(String[] args) {
-		int n = 1;
+		int n = 28;
 //		int n = 2;
 //		int n = 3;
 //		int n = 4;
@@ -11,12 +13,21 @@ public class N12899 {
    
 	public static String solution(int n) {
         String answer = "";
-        StringBuilder builder = new StringBuilder();
-        do {
-        	int number = n % 3;
-        	n /= 3;        	
-
-        }while(n > 0);
+        ArrayList<String> list = new ArrayList<String>();
+        while(n > 0) {
+        	int t = n % 3;
+        	if(t == 0) {
+        		t = 4;
+        		n -= 1;
+        	}
+        	list.add(Integer.toString(t));
+        	n /= 3;
+        }
+        int lastIdx = list.size() - 1;
+        for(int i = lastIdx;i>=0;i--) {
+        	answer += list.get(i);
+        }
+        
         return answer;
     }
 }
