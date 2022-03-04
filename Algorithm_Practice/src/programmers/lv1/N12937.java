a6a89b3d77dca0574fe530c253fba11e78523a58
dyless0811 <dyless0811@gmail.com>
2022-03-04 17:08:49 +0900

    프로그래머스 - 짝수와 홀수

---
+++ Algorithm_Practice/src/programmers/lv1/N12937.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+public class N12937 {
+	public static void main(String[] args) {
+		int num = 3;
+		solution(num);
+	}
+    public static String solution(int num) {
+        
+        return num%2!=0 ? "Odd" : "Even";
+    }
+}
