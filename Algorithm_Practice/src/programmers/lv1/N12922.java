84747c34f0296c070994cc998b1c2b8e6f5a8d91
dyless0811 <dyless0811@gmail.com>
2022-03-04 18:29:57 +0900

    프로그래머스 - 수박수박수박수박수박수?

--- Algorithm_Practice/src/programmers/lv1/N12922.java
+++ Algorithm_Practice/src/programmers/lv1/N12922.java
@@ -1,5 +1,17 @@
 package programmers.lv1;
 
 public class N12922 {
-
+	public static void main(String[] args) {
+		int n = 3;
+		solution(n);
+	}
+    public static String solution(int n) {
+        String watermelon = "수박";
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < n/2+1; i++) {
+			s.append(watermelon);
+		}
+        
+        return s.substring(0, n);
+    }
 }
