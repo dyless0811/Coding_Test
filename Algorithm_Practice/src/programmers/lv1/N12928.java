2df1a5457602dea2f42955003b1cbc1b376b026e
dyless0811 <dyless0811@gmail.com>
2022-03-08 20:10:33 +0900

    프로그래머스 - 약수의 합

---
+++ Algorithm_Practice/src/programmers/lv1/N12928.java
@@ -0,0 +1,20 @@
+package programmers.lv1;
+
+public class N12928 {
+	public static void main(String[] args) {
+		int n = 12;
+		solution(n);
+	}
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+			if(n%i == 0 ) {
+				answer += i;
+				if(n/i != i) {
+					answer += n/i;
+				}
+			}
+		}
+        return answer;
+    }
+}
